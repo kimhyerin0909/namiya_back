4d66d56409c401f03b688aa44fec7feb8870098e
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-07 09:35:48 +0900

    Feat - router 설정, json parser

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.listen(require("./constants/server.constant"));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", require("./routes/auth.route"));
+app.use("/worry", require("./routes/worry.route"));
