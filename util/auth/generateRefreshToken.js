3f7ce27cefd62cc40cea8853edb21f8687d91edd
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-11 13:39:56 +0900

    Chore - jwt key 변경

--- util/auth/generateRefreshToken.js
+++ util/auth/generateRefreshToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (email) => {
-  return jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+  return jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: "30 days",
   });
 };
