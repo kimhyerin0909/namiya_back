a071b08d03be2693ab58d95a8c18e5e85685249a
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-13 13:34:56 +0900

    Feat - 내 나야미 가져오기

--- routes/letter.route.js
+++ routes/letter.route.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const db = require("../config/db.config");
 const jwtController = require("../controller/jwt.controller");
 
 router.get("/all", (req, res) => {
@@ -7,6 +8,16 @@ router.get("/all", (req, res) => {
 
 router.post("/send", (req, res) => {
   jwtController.checkJWT(res, req.headers.authorization.split(" ")[1]);
+});
+
+router.get("/:userId", (req, res) => {
+  const { userId } = req.params;
+  if (jwtController.checkJWT(res, req.headers.authorization.split(" ")[1], userId)) {
+    db.query(`SELECT * FROM Letter WHERE userId = ${userId};`, (err, rows) => {
+      if (rows.length < 1) return res.status(200).json({ message: "나야미가 없습니다." });
+      else return res.status(200).json(rows);
+    });
+  }
 });
 
 module.exports = router;
