ba815822448d8f1eeb765d0401d0b2b6b68796e0
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-11 21:45:22 +0900

    Feat - send letter, get today letter

--- routes/letter.route.js
+++ routes/letter.route.js
@@ -1,13 +1,30 @@
 const router = require("express").Router();
 const db = require("../config/db.config");
 const jwtController = require("../controller/jwt.controller");
+const schedule = require("node-schedule");
 
-router.get("/all", (req, res) => {
-  res.send("worry get");
-});
+const today = new Date();
+const todayStr = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
-router.post("/send", (req, res) => {
-  jwtController.checkJWT(res, req.headers.authorization.split(" ")[1]);
+router.post("/send/:userId", (req, res) => {
+  const { userId } = req.params;
+  const { content, sendAt, previousId } = req.body;
+  if (jwtController.checkJWT(res, req.headers.authorization.split(" ")[1], userId)) {
+    db.query(
+      `SELECT * FROM Letter WHERE userId = ${userId} AND DATEDIFF(sendAt, '${sendAt}') = 0;`,
+      (err, rows) => {
+        if (rows.length > 0) return res.status(400).json({ message: "오늘 나야미가 존재합니다." });
+        else {
+          db.query(
+            `INSERT INTO Letter VALUES (0, ${previousId}, ${userId}, "${sendAt}", "${content}", null)`,
+            () => {
+              res.status(200).json({ message: "성공!" });
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
 router.get("/:userId", (req, res) => {
@@ -18,6 +35,27 @@ router.get("/:userId", (req, res) => {
       else return res.status(200).json(rows);
     });
   }
+});
+
+router.get("/today/:userId", (req, res) => {
+  const { userId } = req.params;
+  if (jwtController.checkJWT(res, req.headers.authorization.split(" ")[1], userId)) {
+    db.query(
+      `SELECT * FROM Letter WHERE userId = ${userId} AND DATEDIFF(sendAt, '${todayStr}') = 0;`,
+      (err, rows) => {
+        if (rows.length > 0)
+          return res
+            .status(200)
+            .json({ isExist: true, message: "오늘 나야미가 존재합니다.", data: rows[0] });
+        else
+          return res.status(200).json({ isExist: false, message: "나야미를 작성할 수 있습니다." });
+      }
+    );
+  }
+});
+
+router.get("/reply/:userId", (req, res) => {
+  const { userId } = req.params;
 });
 
 module.exports = router;
